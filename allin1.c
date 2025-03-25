e942d94c8dd573a6e371e8638fa38b875e4c0661
Anish Rana <anishrana1096@gmail.com>
2025-03-25 09:44:33 +0530

    goto statement

--- allin1.c
+++ allin1.c
@@ -2,12 +2,12 @@
 
 int main() {
     for (int i = 0; i < 15; i++) {           // Loop from 0 to 14
-        if (i == 5) {
-            break;                 
-        printf("i is %d\n", i);               // Print the current value of `i`
+        if (i == 5) {                        // Stop loop when i equals 5
+            break;
+        }
+        printf("i is %d\n", i);              // Print the current value of `i`
     }
 
-    printf("For loop is done!");              // Print this after exiting the loop
-
-    return 0;                                 // Return 0 indicating successful execution
+    printf("For loop is done!\n");           // Print this after exiting the loop
+    return 0;                                // Return 0 indicating successful execution
 }
