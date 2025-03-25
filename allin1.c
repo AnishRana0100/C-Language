963e76185a8fade4b191bf7db76b41465d9ad417
Anish Rana <anishrana1096@gmail.com>
2025-03-25 09:45:01 +0530

    goto statement

--- allin1.c
+++ allin1.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
-int main() {
-    for (int i = 0; i < 15; i++) {           // Loop from 0 to 14
-        if (i == 5) {                        // Stop loop when i equals 5
-            break;
-        }
-        printf("i is %d\n", i);              // Print the current value of `i`
-    }
-
-    printf("For loop is done!\n");           // Print this after exiting the loop
-    return 0;                                // Return 0 indicating successful execution
+int main()
+{
+int num,i=1;
+printf("Enter the no whose table you want to print?");
+scanf("%d",&num);
+table:
+printf("%d x %d = %d\n",num,i,num*i);
+i++;
+if(i<=10)
+goto table;
 }
