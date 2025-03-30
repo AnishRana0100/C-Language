47201c86981d6a11e17ea52b100edf31bc77532a
Anish Rana <anishrana1096@gmail.com>
2025-03-30 16:56:37 +0530

    Marks and grade code using if else if ladder

--- allin1.c
+++ allin1.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 int main()
 {
-int num,i=1;
-printf("Enter the no whose table you want to print?");
-scanf("%d",&num);
-table:
-printf("%d x %d = %d\n",num,i,num*i);
-i++;
-if(i<=10)
-goto table;
+int marks;
+printf("Enter your marks?");
+scanf("%d",&marks);
+if(marks > 85 && marks <= 100)
+{
+printf("Congrats ! you scored grade A ...");
+}
+else if (marks > 60 && marks <= 85)
+{
+printf("You scored grade B + ...");
+}
+else if (marks > 40 && marks <= 60)
+{
+printf("You scored grade B ...");
+}
+else if (marks > 30 && marks <= 40)
+{
+printf("You scored grade C ...");
+}
+else
+{
+printf("Sorry you are fail ...");
+}
+return 0;
 }
